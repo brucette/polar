075027eb069effc04234fba1fd047aa573c03b44
Tina Bruce <tinabruce42@hotmail.com>
2023-06-26 20:26:47 +0200

    Access token request working.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from "cors";
+import fetch from "node-fetch";
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -7,8 +8,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/data', (req, res) => {
-    res.send('Lets see!')
+app.post('/data', async (req, res) => {
+    const { code } = req.body;
+    
+    const inputBody = {
+        'grant_type': 'authorization_code',
+        'code': code
+    };
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'Authorization':'Basic YzA0MmMyMjYtYjZkNS00NTk0LThhZjEtZGM0MzhkNTQyZDZhOmZkZmI3NDBmLWExZmMtNDY2ZS05NjE1LWJkMTRhZmU3ODExNw==',
+    };
+
+    const options = {
+        method: 'POST',
+        body: new URLSearchParams(inputBody),
+        headers: headers
+    };
+
+    const getResponse = async () => {
+
+        const theFetch = await fetch('https://polarremote.com/v2/oauth2/token', options);
+        const theData = await theFetch.json();
+        // console.log('theData', theData)
+        return theData
+    }
+
+    const response = await getResponse()
+    // console.log('response:', response)
+    res.status(200).json(response);
+    
 });
 
 app.listen(port, () => {
--- frontend/src/components/Authorized.js
+++ frontend/src/components/Authorized.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSearchParams } from "react-router-dom";
 
 export const Authorized = () => {
-    // useEffect(() => {
-    //     fetch('http://localhost:8080/oauth2_callback')
-
-    // }
-    // )
+    const [userData, setUserData] = useState({});
+    
     const [searchParams] = useSearchParams();
 
     const code = searchParams.get('code');
     
-    return <h1>Hello! {code}</h1>
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          code
+        })
+    };
+
+    const getData = async () => {
+        const data = await fetch('http://localhost:8080/data', options);
+        console.log('data:', data)
+        // const result = await data;
+        // console.log('result', result)
+        // setUserData(result);
+        // console.log(userData)
+    };
+
+    return (
+        <>
+            Hello! {code}
+            <button 
+                type='button'
+                onClick={getData}>
+                    Get data
+            </button>
+        </>
+    )
 }
